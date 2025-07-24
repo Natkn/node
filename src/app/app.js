04b4aa53eb8aef4b55c251be8d56dfd1540e06b7
Nastya Star <103874674+Natkn@users.noreply.github.com>
2025-07-24 18:42:32 +0300

    com

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,46 @@
 const http = require("http");
+const fs = require("fs");
+const url = require("url");
 
-const server = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end("Hello, World!\n");
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true); // Разбираем URL
+  const query = parsedUrl.query; // Получаем параметры запроса
+
+  if (parsedUrl.pathname !== "/") {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end();
+    return; //  Если путь не "/", возвращаем ошибку 500
+  }
+
+  if (query.hello) {
+    const name = query.hello;
+    if (name) {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end(`Hello, ${name}!`);
+    } else {
+      res.writeHead(400, { "Content-Type": "text/plain" });
+      res.end("Enter a name");
+    }
+  } else if (query.users) {
+    fs.readFile("data/users.json", (err, data) => {
+      if (err) {
+        console.error(err);
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+        return;
+      }
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(data);
+    });
+  } else {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello, World!");
+  }
 });
 
-server.listen(3000, () => {
-  console.log("Сервер запущен по адресу http://127.0.0.1:3000");
+const port = 3003;
+const hostname = "127.0.0.1";
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
